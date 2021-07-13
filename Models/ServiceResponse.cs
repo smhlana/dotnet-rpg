8578a33d421701ddb8a5c75baae0ae9e6b079181
smhlana <smhlana98@gmail.com>
2021-07-13 12:16:28 +0200

    Edit service to get items from the DB.

--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -2,6 +2,8 @@ namespace dotnet_rpg.Models
 {
     public class ServiceResponse<T>
     {
+        public ServiceResponse() { Success = true; }
+
         public T Data { get; set; }
         public bool Success { get; set; }
         public string Message { get; set; }
